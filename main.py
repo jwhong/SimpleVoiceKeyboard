372606690c916dd8b8eeb55025840c70712b6923
James Whong <james.whong@gmail.com>
2021-01-17 16:33:00 -0800

    The Voice keyboard is working really well! In fact I'm writing this commit message with it.

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 
-# NOTE: this example requires PyAudio because it uses the Microphone class
-
 import speech_recognition as sr
 import pyaudio as pa
 from pynput import keyboard
+from time import sleep
+from threading import Semaphore
 
-# Flags for defining behavior
+# Some magic numbers
+CHUNK_SIZE = 1024
+SAMPLE_RATE = 44100
+SAMPLE_WIDTH = 2
+N_CHANNELS = 1
+
+# When true, plays back recorded audio before sending it up for speech recognition
 PLAYBACK=False
 
 def audioToText(audio:sr.AudioData)->str:
@@ -15,7 +21,7 @@ def audioToText(audio:sr.AudioData)->str:
         # for testing purposes, we're just using the default API key
         # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
         # instead of `r.recognize_google(audio)`
-        return r.recognize_google(audio)
+        return recognizer.recognize_google(audio)
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
         return ""
@@ -27,12 +33,9 @@ def audioToText(audio:sr.AudioData)->str:
         return ""
 
 def playAudio(audio:sr.AudioData)->None:
-    # instantiate PyAudio (1)
     p = pa.PyAudio()
 
-    # open stream (2)
-    chunk_size = 1024
-    chunk_size_bytes = chunk_size * audio.sample_width
+    chunk_size_bytes = CHUNK_SIZE * audio.sample_width
     stream = p.open(format=p.get_format_from_width(audio.sample_width),
                     channels=1,
                     rate=audio.sample_rate,
@@ -50,61 +53,86 @@ def playAudio(audio:sr.AudioData)->None:
     # close PyAudio (5)
     p.terminate()
 
-# obtain audio from the microphone
-r = sr.Recognizer()
-
-class KeyComboListener(object):
-    def __init__(self):
+class MyKeyController(object):
+    def __init__(self, listen_for_combo=(keyboard.Key.alt, keyboard.Key.cmd), sem_to_signal=None):
         self.kb = keyboard.Controller()
-        self.key_combo = set((keyboard.Key.alt, keyboard.Key.cmd))
+        self.key_combo = set(listen_for_combo)
         self.pressed_keys = set()
-        self.combo_engaged = False
+        self.combo_pressed = False
+        self.sem_to_signal = sem_to_signal
         listener = keyboard.Listener(
-            on_press=self.on_press,
-            on_release=self.on_release)
+            on_press=self.onPress,
+            on_release=self.onRelease)
         listener.start()
-    def on_press(self, key):
+    def onPress(self, key):
         if key in self.key_combo:
             self.pressed_keys.add(key)
-            if (self.pressed_keys == self.key_combo) and not self.combo_engaged:
-                self.combo_engaged = True
-    def on_release(self, key):
+            if (self.pressed_keys == self.key_combo) and not self.combo_pressed:
+                self.combo_pressed = True
+                if self.sem_to_signal: self.sem_to_signal.release()
+    def onRelease(self, key):
         if key in self.pressed_keys:
             self.pressed_keys.remove(key)
-            if self.combo_engaged:
-                self.combo_engaged = False
+            if self.combo_pressed:
+                self.combo_pressed = False
+    def isComboPressed(self): return self.combo_pressed
 
-keyboard_listener = KeyComboListener()
+def recordWhile(cb_returns_true)->sr.AudioData:
+    p = pa.PyAudio()
+    stream = p.open(format=p.get_format_from_width(SAMPLE_WIDTH),
+                    channels=N_CHANNELS,
+                    rate=SAMPLE_RATE,
+                    input=True,
+                    output=False)
+    # read data
+    frames = []
+    while cb_returns_true():
+        data = stream.read(CHUNK_SIZE)
+        frames.append(data)
 
-print("Listening...")
-while True:
-    with sr.Microphone() as mic:
-        #print("Energy threshold before tuning: ", r.energy_threshold)
-        #r.adjust_for_ambient_noise(mic, 1.0)  # listen to calibrate the energy threshold for ambient noise levels
-        #print("Energy threshold after tuning: ", r.energy_threshold)
-        r.dynamic_energy_threshold = False
-        r.energy_threshold = 5000.0  # Fixed for now
-        print("Listening...")
-        try:
-            audio = r.listen(mic, timeout=5.0)
-        except sr.WaitTimeoutError:
-            continue
-    audio_length = len(audio.frame_data)/(audio.sample_rate*audio.sample_width)
-    print("Received audio of length %0.1f"%audio_length)
-    if PLAYBACK:
-        print("Playing back...")
-        playAudio(audio)
-    text = audioToText(audio)
-    if text:
-        print("Sending to keyboard: ", text)
-        kb.type(text)
+    # stop stream (4)
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
 
-if 0:
-    # recognize speech using Google Cloud Speech
-    GOOGLE_CLOUD_SPEECH_CREDENTIALS = r"""INSERT THE CONTENTS OF THE GOOGLE CLOUD SPEECH JSON CREDENTIALS FILE HERE"""
-    try:
-        print("Google Cloud Speech thinks you said " + r.recognize_google_cloud(audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS))
-    except sr.UnknownValueError:
-        print("Google Cloud Speech could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Cloud Speech service; {0}".format(e))
+    # Stitch together into AudioData
+    return sr.AudioData(b''.join(frames), SAMPLE_RATE, SAMPLE_WIDTH)
+
+class MyTextFormatter(object):
+    SENTENCE_ENDINGS = ('.','?','!')
+    def __init__(self):
+        self.capitalization_due = True
+    def process(self, input:str)->str:
+        if not input: return input
+        working = list(input)
+        if self.capitalization_due:
+            working[0] = working[0].capitalize()
+            self.capitalization_due = False
+        if working[-1] in MyTextFormatter.SENTENCE_ENDINGS:
+            self.capitalization_due = True
+        working.append(' ')
+        return ''.join(working)
+
+
+if __name__ == "__main__":
+    # obtain audio from the microphone
+    sem = Semaphore(0)
+    recognizer = sr.Recognizer()
+    key_controller = MyKeyController(sem_to_signal=sem)
+    formatter = MyTextFormatter()
+
+    while True:
+        sem.acquire()
+        print("Recording...")
+        audio = recordWhile(key_controller.isComboPressed)
+        audio_length = len(audio.frame_data) / (audio.sample_rate * audio.sample_width)
+        print("Received audio of length %0.1fs" % audio_length)
+        if PLAYBACK:
+            print("Playing back...")
+            playAudio(audio)
+        text = audioToText(audio)
+        if text:
+            text = formatter.process(text)
+            print("Sending to keyboard: ", text)
+            key_controller.kb.type(text)
+        print("Going back to sleep.")
